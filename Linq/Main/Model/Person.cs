8fed056c7342513ff76bd3760fb90119075a9c59
Ahmed <82724393+ahmed-git-tn@users.noreply.github.com>
2023-10-08 13:29:22 +0200

    Add json data

---
+++ Linq/Main/Model/Person.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Main.Model
+{
+    internal record Person
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+    }
+}
--- Linq/Main/Program.cs
+++ Linq/Main/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Main.Model;
+using System.Text.Json;
+
+var file = File.ReadAllText(@".\Data\MOCK_DATA_PERSONNES.json");
+var list = JsonSerializer.Deserialize<List<Person>>(file);
+var orderedByFirstName = list.OrderBy(p => p.first_name);
+foreach (var item in orderedByFirstName)
+{
+    Console.WriteLine(item.first_name + " " + item.last_name+ " " + item.email);
+    await Task.Delay(500);
+}
